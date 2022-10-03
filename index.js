8315d8579bf0e82290bbac5c1b87227f77ccb0e2
Ward Van Driessche <wardvandriessche@gmail.com>
2022-10-03 20:12:43 +0200

    Add js function to load job page

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function imgToSmile(){
 function imgToNormal(){
     document.getElementById("header-img").src = "images/ward-nobkg.png"
 }
+
+function jobPage(){
+    window.location.href="pages/job.html";
+}
