3d3e181a194157401632bf9c3956644c96ae69b2
Ward Van Driessche <wardvandriessche@gmail.com>
2022-10-12 17:18:07 +0200

    Fix navbar + add quote Victor

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
-function imgToSmile(){
+function imgToSmile() {
     document.getElementById("header-img").src = "images/ward-nobkg-smile.png"
 }
 
-function imgToNormal(){
+function imgToNormal() {
     document.getElementById("header-img").src = "images/ward-nobkg.png"
 }
 
-function jobPage(){
-    window.location.href="pages/job.html";
+function jobPage() {
+    window.location.href = "pages/job.html";
 }
 
-function loadNavBar(){
+function loadNavBar() {
     fetch('navbar.html')
-    .then(response=> response.text())
-    .then(text=> document.getElementById('navbar').innerHTML = text);
-  }
+        .then(response => response.text())
+        .then(text => document.getElementById('navbar').innerHTML = text);
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
